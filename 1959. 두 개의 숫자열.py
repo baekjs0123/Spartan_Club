905e3eb9f5a58161e3e1d35e2957aad84c7931af
BaekJongseok <baekjs0123@naver.com>
2025-03-05 17:59:20 +0900

    0305 1959. 두 개의 숫자열 풀이중

--- 1959. 두 개의 숫자열.py
+++ 1959. 두 개의 숫자열.py
@@ -1,5 +1,23 @@
+# 테스트 케이스 갯수
 T = int(input())
+# T만큼 반복
 for tc in range(1, T + 1):
+    # N: Ai의 길이, M: Bj의 길이
     N, M = map(int, input().split())
+    # Ai 입력받기
     Ai = list(map(int, input().split()))
+    # Bj 입력받기
     Bj = list(map(int, input().split()))
+    len_A = len(Ai)
+    len_B = len(Bj)
+    short_arr = 0
+    long_arr = 0
+    if len_A < len_B:
+        short_arr = Ai
+        long_arr = Bj
+    else:
+        short_arr = Bj
+        long_arr = Ai
+
+
+
